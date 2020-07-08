b8c9644203592d4eefbc0b1a4c107d9cd22957ee
Josh <mrjoshuasperry@gmail.com>
2020-07-08 00:46:09 -0400

    Remove unused python file

--- src/assets/fish/new text document.py
+++
@@ -1,9 +0,0 @@
-import os
-
-paths = (os.path.join(root, filename)
-        for root, _, filenames in os.walk('E:\\temp\\fish')
-        for filename in filenames)
-
-for path in paths:
-    newname = path.replace("abc", "");
-    os.rename(path, newname)
